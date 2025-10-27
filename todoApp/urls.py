7c21e2e4c8341ebcb910bb3095ab9187aca540f7
Natalia <natalia.mat19@gmail.com>
2025-10-27 20:37:04 +0100

    Initial commit

---
+++ todoApp/urls.py
@@ -0,0 +1,32 @@
+"""todoApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
+from todos import views_auth
+from . import settings, views
+
+urlpatterns = [
+    path('todos/', include('todos.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('signup/', views_auth.signup, name='signup')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
---
+++ todos/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .models import Todo
+from django.http import HttpResponseRedirect
+
+@method_decorator(login_required, name='dispatch')
+class IndexView(generic.ListView):
+    template_name = 'todos/index.html'
+    context_object_name = 'todo_list'
+
+    def get_queryset(self):
+        """Return all the latest todos."""
+        return Todo.objects.filter(user=self.request.user).order_by('-created_at')
+
+@login_required
+def add(request):
+    title = request.POST['title']
+    Todo.objects.create(title=title, user=request.user)
+
+    return redirect('todos:index')
+
+@login_required
+def delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id, user=request.user)
+    todo.delete()
+
+    return redirect('todos:index')
+
+@login_required
+def update(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id, user=request.user)
+    isCompleted = request.POST.get('isCompleted', False)
+    if isCompleted == 'on':
+        isCompleted = True
+    
+    todo.isCompleted = isCompleted
+
+    todo.save()
+    return redirect('todos:index')
