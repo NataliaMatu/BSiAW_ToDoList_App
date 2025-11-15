2b565cc3259c304c4ec085bf80d7c344bc6dcdcd
Natalia <natalia.mat19@gmail.com>
2025-11-15 23:07:04 +0100

    Dodaje sesje

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',   # <-- standardowe sesje DJANGO
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
