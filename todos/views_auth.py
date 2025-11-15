3ae8fc77146333458f4fbf54632adf57e16dab7b
Natalia <natalia.mat19@gmail.com>
2025-11-15 19:36:15 +0100

    Update sesji blagam ostatni raz

--- todos/views_auth.py
+++ todos/views_auth.py
@@ -14,9 +14,6 @@ class CustomLoginView(LoginView):
         browser_id = self.request.GET.get("browser_id")
         response = super().form_valid(form)
 
-        if not self.request.session.session_key:
-            self.request.session.save()
-
         if browser_id:
             self.request.session["_browser_id"] = browser_id
             self.request.session.save()
@@ -43,8 +40,8 @@ def session_check(request):
 
     try:
         data = json.loads(request.body.decode("utf-8"))
-    except Exception:
-        return JsonResponse({"valid": False})
+    except:
+        return JsonResponse({"valid": True})
 
     browser_id = data.get("browser_id")
     session_browser_id = request.session.get("_browser_id")
